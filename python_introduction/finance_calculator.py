1477221f40f989e8873e3e93a7e5119d972a09f3
Magara Hesborn <magarahesborn1@gmail.com>
2025-05-20 17:10:35 +0300

    Made changes to ensure integer output

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@ expenses = int(input("Enter your total monthly expenses: "))
 
 monthly_savings = income - expenses
 
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+projected_savings = monthly_savings * 12 + int((monthly_savings * 12 * 0.05))
 
 print(f"Your monthly savings are {monthly_savings}.")
-print(f"Projected savings after one year, with intrest, is: {projected_savings}")
+print(f"Projected savings after one year, with intrest, is: {projected_savings}.")
