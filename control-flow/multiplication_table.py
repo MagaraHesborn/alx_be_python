0fc810852e8ff87fd1ee66bddcb2e771e5c2458d
Magara Hesborn <magarahesborn1@gmail.com>
2025-05-30 10:36:38 +0300

    created a script that to generate multiplication table for a given number

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# multplication table
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for multiple in range(1, 11):
+    print(f"{number} * {multiple} = {number * multiple}")
+    
