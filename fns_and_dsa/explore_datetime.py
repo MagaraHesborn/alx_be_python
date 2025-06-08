77103740f5b5847dd04cfbc44f5eed1dc82a5de3
Magara Hesborn <magarahesborn1@gmail.com>
2025-06-08 14:14:36 +0300

    implemented a script using datetime module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,19 @@
-from datetime import date, timedelta
-
+from datetime import datetime, timedelta, date
 def display_current_datetime():
-    current_date = date.today()
+    current_date = datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"{current_date}")
 
-display_current_datetime()
+def calculate_future_date():
+    number_of_days = int(input("Enter the number of days to add: "))
+    current_date = date.today()
+    futrure_date = current_date + timedelta(days=number_of_days)
+    futrure_date = futrure_date.strftime("%Y-%m-%d")
+    print(f"{futrure_date}")
 
+
+
+   
+
+display_current_datetime()
+calculate_future_date()
