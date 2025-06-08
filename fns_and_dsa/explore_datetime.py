cb42230ad11da9698f5c88b9ce661cfb65ce5cf2
Magara Hesborn <magarahesborn1@gmail.com>
2025-06-08 13:23:33 +0300

    aded time delta

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,8 @@
-import datetime
+from datetime import date, timedelta
 
 def display_current_datetime():
-    current_date = datetime.date.today()
+    current_date = date.today()
+    print(f"{current_date}")
 
 display_current_datetime()
 
