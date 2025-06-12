a3ab604de057bf74e857d12b00ba374a3f16269d
Magara Hesborn <magarahesborn1@gmail.com>
2025-06-12 12:05:23 +0300

    Created a bank account class that deposits, withdraws and displays balance

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, balance=0):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+        else:
+            print("Enter a valid amount to deposit.")
+    
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            self.__balance = self.__balance  # No change to balance
+            return False
+        else:
+            self.__balance -= amount
+            return True
+            
+    def display_balance(self):
+        print(f"Current balance: ${self.__balance}")
